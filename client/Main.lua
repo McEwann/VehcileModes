8951ee40393ffed187ea3e9f8ff4a46ba59bdd97
McEwan <craigmcewandouglas60@gmail.com>
2021-08-12 00:33:07 +0100

    Cleaned up and commented

--- client/Main.lua
+++ client/Main.lua
@@ -1,43 +1,35 @@
 
-local pursuitmode = true -- on/off speed
+local pursuitmode = true -- Toggle
 local toggle = 118 -- Numpad 9
 local vehicle = GetVehiclePedIsIn(player)
-local Interceptor_hash = GetHashKey("2015polstang")
+local Interceptor_hash = GetHashKey("2015polstang")--Get Hash of mustang / pursuit vehicle
 
-
--- Thread
 Citizen.CreateThread(function()
-
-
-	while true do
-    Citizen.Wait(0)
+while true do
+Citizen.Wait(0)
     if IsControlJustPressed(1, 118) and IsPlayerInInterceptor() then
-        pursuitmode = not pursitmode
-
-			if pursuitmode then
-				TriggerEvent("chatMessage", 'persuit mode')
+        pursuitmode = not pursuitmode
+            if pursuitmode then
+				TriggerEvent("chatMessage", 'Pursuit Mode')
                 while pursuitmode do
                     Citizen.Wait(0)
-                    
-                    SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 20.0)
-                    SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 20.0)
-                    print("weewoo")
-
+                    --Vehicle modifiers must be looped every tick
+                    SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.5)
+                    SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.5)
+                    --Break Loop
                     if IsControlJustPressed(1, 118) then
-                        TriggerEvent("chatMessage", 'Normal')
+                        TriggerEvent("chatMessage", 'Patrol Mode')
                         pursuitmode = false 
                     end
                 end
 			else
 				TriggerEvent("chatMessage", 'Normal')
-                SetVehicleCheatPowerIncrease(GetVehiclePedIsIn(ped, false), 1)
-
             end
 		end
 	end	
 end)
 
--- Function
+-- Functions
 function IsPlayerInInterceptor()
 	local lPed = GetPlayerPed(-1)
 	local vehicle = GetVehiclePedIsIn(lPed)
