dfe1ca2895d5bb2b92e69f86513e84b06d599878
McEwan <craigmcewandouglas60@gmail.com>
2021-08-11 21:30:08 +0100

    Added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'VehicleModes'
+description 'Custom Script to allow changing between handling modes of cars'
+author 'McEwan'
+version 'v 1.0'
+
+client_scripts {
+    'Client.lua',
+}
+
+server_scripts {
+    'Server.lua',
+}
