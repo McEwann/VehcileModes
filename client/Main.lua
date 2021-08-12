bab50a8ec2be6cef9761916f2ad723c1d9ff6879
McEwan <craigmcewandouglas60@gmail.com>
2021-08-12 12:52:42 +0100

    Cleaned up

--- client/Main.lua
+++ client/Main.lua
@@ -1,27 +1,28 @@
 
 local pursuitmode = false -- Toggle
-local toggle = 118 -- Numpad 9
+local vehmode_key = 172  -- Numpad 9
 local vehicle = GetVehiclePedIsIn(player)
 local Interceptor_hash = GetHashKey("2015polstang")--Get Hash of mustang / pursuit vehicle
 
 Citizen.CreateThread(function()
 while true do
 Citizen.Wait(0)
-    if IsControlJustPressed(1, 118) and IsPlayerInInterceptor() then
+
+    if IsDisabledControlPressed (1, 36) and IsControlJustPressed(1, vehmode_key) and IsPlayerInInterceptor() then
         pursuitmode = not pursuitmode
             if pursuitmode then
-				TriggerEvent("chatMessage", 'Pursuit Mode')
+				TriggerEvent("chatMessage", 'Vehicle Mode: Pursuit')
                 while pursuitmode do
                     Citizen.Wait(0)
                     --Vehicle modifiers must be looped every tick
                     SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.5)
                     SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.5)
-                    print("test")
                     
                     --Break Loop
-                    if IsControlJustPressed(1, 118) or IsPedOnFoot(PlayerPedId()) then
-                        TriggerEvent("chatMessage", 'Patrol Mode')
+                    if  IsDisabledControlPressed (1,36) and IsControlJustPressed(1, 173) or IsPedOnFoot(PlayerPedId()) then
+                        TriggerEvent("chatMessage", 'Vehicle Mode: Patrol')
                         pursuitmode = false 
+                        
                     end
                 end
             end
@@ -29,9 +30,14 @@ Citizen.Wait(0)
 	end	
 end)
 
+
+
+
+
 -- Functions
 function IsPlayerInInterceptor()
 	local lPed = GetPlayerPed(-1)
 	local vehicle = GetVehiclePedIsIn(lPed)
 	return IsVehicleModel(vehicle, Interceptor_hash)
 end
+
